f97ca5b0e87b9b6e2c913346169bc0d4636dd31d
Scott Hurlow <shurlow@macalester.edu>
2013-02-28 11:20:27 -0600

    better

--- src/edu/macalester/cs124/guessing/GameMaster.java
+++ src/edu/macalester/cs124/guessing/GameMaster.java
@@ -2,26 +2,18 @@ package edu.macalester.cs124.guessing;
 
 import java.util.Random;
 
-import acm.program.ConsoleProgram;
-
-public class GameMaster extends ConsoleProgram {
+public class GameMaster {
 	
 	public GameMaster() {
 	}
-	
-//	public void run() {
-//		startNewGame();
-//		println(rand);
-//		println(submitGuess(95));
-//	}
 	
 	public void startNewGame(){
 		Random randomGenerator = new Random();
 		rand = randomGenerator.nextInt(100);
 	 }
 	 
-	 public static int submitGuess(int guess){
-		 COUNT += 1;
+	 public int submitGuess(int guess){
+		 count += 1;
 		 if(guess < rand){
 			 return LOW;
 		 } else if (guess > rand){
@@ -30,15 +22,15 @@ public class GameMaster extends ConsoleProgram {
 			 return CORRECT;
 	 }
 	 
-	 public static int getGuessCount(){
-		 return COUNT;
+	 public int getGuessCount(){
+		 return count;
 		 
 	 }
 	
 		
 	public static final int LOW = -1, HIGH = 1, CORRECT = 0;
 	public static int rand;
-	private static int COUNT;
+	private int count;
 	
 	
 	
--- src/edu/macalester/cs124/guessing/Player.java
+++ src/edu/macalester/cs124/guessing/Player.java
@@ -1,15 +1,12 @@
 package edu.macalester.cs124.guessing;
 
 public class Player {
-	public Player(){
-		
-	}
 	
 	public void startNewGame(){
 		guess = 0;
 	}
 	
-	public static int nextGuess(){
+	public int nextGuess(){
 		return guess;	
 	}
 	
@@ -22,5 +19,5 @@ public class Player {
 		
 	}
 
-	public static int guess;
+	public int guess;
 }
